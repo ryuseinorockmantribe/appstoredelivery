1a2330d053ca0a86a553eb537806721b6eebd941
Jose Alberto Aramburo Lucero <josealbertoaramburolucero@Joses-MacBook-Air.local>
2023-07-29 15:14:27 -0600

    first commit

---
+++ app/src/main/java/com/optic/deliverykotlinudemy/config/Config.kt
@@ -0,0 +1,9 @@
+package com.optic.deliverykotlinudemy.config
+
+class Config {
+
+    companion object {
+        val PAYPAL_CLIENT_ID = "ATq5VzhRwEWqeLUfJODbu_7jV_YRAb0t5tUcFN8LEA2_HQdcikrhOUbXRlN-1QcTvYRRFWnxM4AbT9hj"
+    }
+
+}
